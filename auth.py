ea74e3adb8ce8d08375e6d29ab4a2aee8521f9a9
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-26 23:47:38 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,15 +1,13 @@
 from flask import session, redirect, url_for
-import csv, os
+import csv
 
-ADMIN_FILE = "data/admins.csv"
+def company_login(email, password):
+    with open("data/companies.csv", newline="", encoding="utf-8") as f:
+        for r in csv.reader(f):
+            if r[1] == email and r[2] == password:
+                return r
+    return None
 
-def check_login(username, password):
-    with open(ADMIN_FILE, newline="", encoding="utf-8") as f:
-        for row in csv.reader(f):
-            if row[0] == username and row[1] == password:
-                return True
-    return False
-
-def login_required():
-    if not session.get("admin"):
+def login_required(role):
+    if session.get("role") != role:
         return redirect(url_for("login"))
