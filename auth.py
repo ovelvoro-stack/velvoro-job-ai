bb5d1f13523961ecf7458a187d03424a5d4a7ec2
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-26 13:47:07 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,7 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def hash_password(password):
-    return pwd_context.hash(password)
+    return generate_password_hash(password)
 
-def verify_password(password, hashed):
-    return pwd_context.verify(password, hashed)
+def verify_password(hash, password):
+    return check_password_hash(hash, password)
