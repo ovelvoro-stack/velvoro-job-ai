df3c3973b4ad3b10fb80b5853af4f8221efe213b
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-26 22:41:03 +0530

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,36 @@
+QUESTIONS = {
+    "IT": [
+        {
+            "q": "Python is used for?",
+            "options": ["Web", "AI", "Data", "All"],
+            "answer": "All"
+        },
+        {
+            "q": "HTML stands for?",
+            "options": ["Hyper Text Markup Language", "High Text", "None", "Hyper Tool"],
+            "answer": "Hyper Text Markup Language"
+        },
+        {
+            "q": "Which is backend?",
+            "options": ["Python", "HTML", "CSS", "Bootstrap"],
+            "answer": "Python"
+        }
+    ],
+    "Non-IT": [
+        {
+            "q": "Sales goal means?",
+            "options": ["Target", "Profit", "Loss", "Offer"],
+            "answer": "Target"
+        },
+        {
+            "q": "HR means?",
+            "options": ["Human Resource", "High Risk", "Hire Role", "None"],
+            "answer": "Human Resource"
+        },
+        {
+            "q": "Customer support is?",
+            "options": ["Service", "Product", "Code", "None"],
+            "answer": "Service"
+        }
+    ]
+}
