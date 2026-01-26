0cae4e24e106965f779ba5194de8bf0a3a1de335
ovelvoro-stack <ovelvoro@gmail.com>
2026-01-26 15:50:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
 
-SECRET_KEY = "velvoro_super_secret_key"
-UPLOAD_FOLDER = "static/resumes"
+SECRET_KEY = "velvoro_secret_key"
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "velvoro@123"
+
+RAZORPAY_KEY_ID = "rzp_test_xxxxx"
+RAZORPAY_KEY_SECRET = "xxxxx"
+
+EXCEL_DB = "velvoro_candidates.xlsx"
+UPLOAD_FOLDER = "uploads/resumes"
